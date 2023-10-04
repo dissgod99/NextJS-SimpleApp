6c9e623bd44ae57f2a32e468fcf399386f18f393
Aziz Chikh <azizchpay99@outlook.com>
2023-10-04 22:39:57 +0200

    Add Background color hover effect

--- nextjs-experiment/app/Movie.js
+++ nextjs-experiment/app/Movie.js
@@ -6,7 +6,7 @@ function Movie(props) {
     const imagePath= "https://image.tmdb.org/t/p/original"
     console.log(props.poster_path)
   return (
-    <div key={props.id} className='bg-red-50 p-3 rounded-md relative group hover-scale'>
+    <div key={props.id} className='bg-red-50 p-3 rounded-md relative group hover-scale hover-background'>
         <h1 className='text-black font-bold'>{props.title}</h1>
         <h2 className='text-black'>{props.release_date}</h2>
         <Link href={`/${props.id}`}>
